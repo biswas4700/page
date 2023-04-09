9b3bb10651dcb9a38e94e24932db5be99196526a
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-09 09:36:04 -0400

    Cl

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "";
+const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqa3M4VThxa1pXbV9GeTE2dXhMdmpDNHN3UHZTQXxBQ3Jtc0ttelg4TGFvb0Z4VnNqRDN5NGFGLTFhQVdOTVJ1ZV9qM0xkWGNiU3dZWlF4NGd0RE9zcC1pRTdXMW5NeEFGQTNPb0Jjb3lveFZYWDNncS1JUTU1bnl5RmtCTHNiR1NpWEkxNHFtMGZSSTlZV1N6eXJ4Yw&q=https%3A%2F%2Fglowingc.netlify.app%2F&html_redirect=1";
